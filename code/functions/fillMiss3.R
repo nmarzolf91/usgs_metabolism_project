c81f47dadd987e785eade2c7971ce0e6ac6e8b49
Nick Marzolf <Nick.Marzolf@jonesctr.org>
2024-03-27 15:10:38 -0400

    create v4.Rmd and update fill_and_normalize and fillMiss functions

--- code/functions/fillMiss3.R
+++ code/functions/fillMiss3.R
@@ -1,43 +1,82 @@
 fillMiss3 <- function (dataset, var, block = 30, pmiss = 40, model = "trend", 
                        smooth = TRUE, plot = TRUE, ...) 
 {
+  
+  # modified function from Bernhardt et al. 2022, which modifies code from USGS-R/waterData/fillMiss.R
+  ## https://github.com/USGS-R/waterData/blob/main/fillMiss.R
+  
+  # convert the dataset to a data frame; the downstream functions struggle with tidyverse derived objects so we'll convert at the start
+  dataset <- data.frame(dataset)
+  
+  # create a T/F for if the dataset contains NAs
   pck <- is.na(dataset[, var])
+  
+  # if there are missing data (or this statement is true)
   if (all(pck == TRUE) == FALSE) {
+    
+    # and there are more than 0 missing values
     if (sum(pck) > 0) {
+      # calculate the percentage of missing data in the time-series
       percent <- round((sum(pck)/length(dataset[, var])) * 
                          100, digits = 2)
+      # and figure out where those missing sequences are in the dataset
       rles <- rle(is.na(dataset[, var]))
+      # determine what the longest string of missing data are
       max.mis <- max(rles$lengths[rles$values])
-    }
-    else {
+      
+      # if there are no missing data, by default these values are 0
+    } else {
       percent <- 0
       max.mis <- 0
     }
-    if (percent >= pmiss | max.mis >= block) {
-    }
+    
+    # if (percent >= pmiss | max.mis >= block) {
+    # }
+    
+    # if the percent missing and less than the missing threshold and the longest block of missing is less than the block threshold
     if (percent < pmiss & max.mis < block) {
-      my.series <- window(dataset[, var])
+      
+      # create a time series object with NAs for missing data
+      my.series <- window(ts(dataset[, var]))
+      
+      # if the first entry in the time series is NA, replace it with a LOCF gap fill
       if (is.na(my.series)[1]) {
         my.series[1] <- zoo::na.locf(my.series, option = "nocb", 
                                      na.remaining = "rev")[1]
       }
+      
+      # try to fit a structural time series model to the data
       struct_try <- try(StructTS(my.series, type = model), 
                         silent = TRUE)
+      
+      # if the try statement worked (or there is no try error), fit the model
       if (class(struct_try)[1] != "try-error") {
         my.struct <- StructTS(my.series, type = model)
       }
+      
+      # else use the level arguement in the time series model fit
       if (class(struct_try)[1] == "try-error") {
         message(paste0("type = level was used for ", 
                        var))
         my.struct <- StructTS(my.series, type = "level")
       }
-      if (smooth) 
+      
+      # if we want to smooth, apply the smoother function
+      if (smooth){ 
         fit <- tsSmooth(my.struct)
-      else fit <- fitted(my.struct)
+      } else {
+        fit <- fitted(my.struct)
+      }
+      
+      # extract min and max from the dataset
       data_min <- min(dataset[, var], na.rm = TRUE)
       data_max <- max(dataset[, var], na.rm = TRUE)
+      
+      # change values that are less than the min to the min and greater than the max to the max
       fit[fit[, 1] < data_min, 1] <- data_min
       fit[fit[, 1] > data_max, 1] <- data_max
+      
+      # plot the fitted data over the raw data if we want to 
       if (plot == TRUE) {
         plot(my.series, typ = "l", lwd = 4, xlab = "Observation", 
              ylab = "Observed and estimated times series", 
@@ -48,11 +87,17 @@ fillMiss3 <- function (dataset, var, block = 30, pmiss = 40, model = "trend",
                                            "green"), lwd = c(4, 1), bty = "n", ncol = 2, 
                cex = 0.8)
       }
+      
+      # append the filled data to the dataset where gap filling was applied
       dataset$filled <- dataset[, var]
       dataset$filled[pck] <- fit[pck, 1]
     }
+    
+    # and return the data
     return(dataset[, "filled"])
   }
+  
+  # if there was nothing but missing data, return this error message
   if (all(pck == TRUE) == TRUE) {
     message("Input variable contained only NA values so only NA values were returned")
     return(rep(NA, nrow(dataset)))
--- code/functions/fill_and_normalize_metab.R
+++ code/functions/fill_and_normalize_metab.R
@@ -1,35 +1,116 @@
 fill_and_normalize_metab <- function(df) {
   
+  # source in functions
   source('code/functions/fillMiss3.R')
   source('code/functions/znorm.R')
+  
+  # define the number of sites
+  sites <- unique(df$site)
+  
+  # and years
+  years <- unique(df %>% 
+                    dplyr::mutate(year = lubridate::year(date)) %>% 
+                    dplyr::pull(year))
+  
+  # create a data frame to loop around and to build out as a summary table
+  df_NA <- data.frame(site = rep(sites, each = length(years)),
+                      year = rep(years, times = length(sites)))
+  
+  # where to save output data
   out <- list()
   
-  for(i in 1:length(unique(df$site))) {
+  # build for loop around site-year (or rows in our tracker data frame)
+  for(i in 1:nrow(df_NA)) {
     
-    site_use <- unique(df$site)[i]
+    # extract the row of interest
+    deets <- df_NA[i,]
     
-    df_fill <- try(df %>% 
-                     dplyr::filter(site == site_use) %>% 
-                     #select(date, GPP) %>% 
-                     data.frame() %>% 
-                     dplyr::mutate(GPP_filled = fillMiss3(., var = "GPP", plot = FALSE),
-                                   ER_filled = fillMiss3(., var = 'ER', plot = FALSE))
-                   )
+    # and its site and year
+    site <- deets['site']
+    year <- deets['year']
     
-    if(inherits(df_fill,'try-error'))
+    # filter the larger data frame to this site-year
+    df_use <- df %>% 
+      dplyr::mutate(year = lubridate::year(date)) %>% 
+      dplyr::filter(site %in% !!site,
+                    year %in% !!year)
+    
+    # if this results in an empty data frame, we record that and go to the next step in the loop
+    if(nrow(df_use) == 0){
+      df_NA[i, 'start_date'] <- NA
+      df_NA[i, 'per_NA'] <- NA
+      df_NA[i, 'complete_year'] <- FALSE
       next
+    }
     
-    df_norm <- df_fill %>% 
-      dplyr::mutate(GPP_filled_norm = znorm(., 'GPP_filled'),
-                    ER_filled_norm = znorm(., 'ER_filled'),
-                    NEP = GPP + ER,
-                    NEP_filled = GPP_filled + ER_filled,
-                    NEP_filled_norm = GPP_filled_norm + ER_filled_norm)
+    # pad out the number of days
+    ## the cleaned and filtered model outputs (river_metab in the Rmd) takes away the 'bad' or missing days
+    ## the padr::pad() function expands the time series back to Jan 1 of the given year by day
+    df_pad <- df_use %>% 
+      padr::pad(start_val = lubridate::date(glue::glue("{year}-01-01")),
+                end_val = lubridate::date(glue::glue("{year}-12-31")),
+                'day') %>% 
+      tidyr::fill(site, resolution, year, 
+                  .direction = 'downup')
     
-    out[[i]] <- df_norm
+    # with the padded dataset
+    ## whats the start date (of GPP, not padded GPP)
+    start_date <- df_pad %>% 
+      dplyr::filter(!is.na(GPP)) %>% 
+      dplyr::pull(date) %>% 
+      dplyr::first()
     
-  } # end for loop
+    end_date <- df_pad %>% 
+      dplyr::filter(!is.na(GPP)) %>% 
+      dplyr::pull(date) %>% 
+      dplyr::last()
+    
+    # how many NAs are there (# and percent)
+    NAs <- sum(is.na(df_pad$GPP))
+    per_NA <- round(NAs/length(df_pad$GPP)*100, digits = 2)
+    
+    # record these values in our tracker dataframe
+    df_NA[i, 'start_date'] <- start_date
+    df_NA[i, 'per_NA'] <- per_NA
+    
+    # now that we've padded the dataset, we should be able to gap fill for all time points
+    # however, this is an issue if there is a large gap at the start or end of the time series (ie early or late in the year)
+    ## the gap fill model below takes the first estimates of GPP and back calculates in time what it thinks. But we know rivers are highly seasonal and this can be a bad approach
+    # this if statement checks if the first date of GPP measurements is within the first 90 days of the padded dataset
+    ## if there is >90 missing days at the start, we don't gap fill
+    ### this is a subjective call and maybe worth exploring the window here a bit. 90 days roughly corresponds to 25% of a year
+    if(!start_date %in% seq(dplyr::pull(df_pad[1,'date']),dplyr::pull(df_pad[90,'date']), by = 1) ||
+       !end_date %in% seq(dplyr::pull(df_pad[nrow(df_pad) - 90,'date']),dplyr::pull(df_pad[nrow(df_pad),'date']), by = 1)){
+      df_NA[i, 'complete_year'] <- FALSE
+      next
+    } else {
+      df_NA[i, 'complete_year'] <- TRUE
+    }
+    
+    # here we gap fill
+    ## we are catching if (somehow) a dataset with >40% missing data has passed through and ignore. 40% missing (or 60% data) is the thresold used in Bernhardt et al. (2022) and we use that here
+    ## the block and pmiss parameters also address the size and amount of gaps, but are accounted for upstream in the code
+    # the fillMiss3 function has roots in github.com/USGS-R/waterData/fillMiss (https://github.com/USGS-R/waterData), modified by Bernhardt et al. (2022, PNAS)
+    ## the function looks for the number and sizes of gaps such that they meet the criteria defined and fit and smooth missing data through a structural state-space time series model (tsSmooth)
+    # gap filling methods are many and come with pros and cons. We apply this method given use on similar data (Bernhardt et al. 2022, PNAS)
+    if(per_NA < 40){
+      df_pad$GPP_filled <- fillMiss3(dataset = data.frame(df_pad), 
+                                     var = 'GPP', 
+                                     block = 125,
+                                     pmiss = 40, 
+                                     plot = FALSE)
+      
+      out[[i]] <- df_pad
+      
+    } # end if statement
+    
+  } # end  for loop
   
-  do.call(dplyr::bind_rows, out)
+  # print and save the tracker
+  readr::write_csv(df_NA,
+                   'data/summary_site-year_NAs.csv')
   
+  # return the compiled object
+  out <- do.call(rbind, out)
+
 } # end function
